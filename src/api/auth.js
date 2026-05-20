8daffd2a4af556697a2c16d5a9c10d96305e6b3d
Alejandro Saura Ayllón <1919610@alu.murciaeduca.es>
2026-05-20 13:48:39 +0200

    Remove unused auth API methods
    Removed unused authentication API methods.

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,11 +1,6 @@
 import api from './axios'
- 
 export const register = (data) => api.post('/register', data)
- 
 export const login = (data) => api.post('/login', data)
- 
 export const logout = () => api.post('/logout')
- 
 export const updateProfile = (data) => api.put('/profile', data)
- 
 export const changePassword = (data) => api.put('/password', data)
