b28b0c68064a4c5886081fe0d57433e9babf9aed
Alejandro Saura Ayllón <1919610@alu.murciaeduca.es>
2026-06-04 03:56:45 +0200

    Enhance Category model with UUIDs and fillable fields
    Added UUID support and updated fillable properties.

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -2,14 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name', 'slug'];
+    use HasUuids;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    protected $fillable = ['name'];
 
     public function games()
     {
-        return $this->belongsToMany(Game::class);
+        return $this->belongsToMany(Game::class, 'game_category', 'category_id', 'game_id');
     }
 }
